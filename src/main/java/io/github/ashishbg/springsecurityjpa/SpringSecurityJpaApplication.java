47a3c53de478888e8b1ba5f6e423c0003c2b9d2c
ashishbg <ashishbg.g@gmail.com>
2022-10-09 17:33:56 +0530

    spring security implementation completed with JPA

--- src/main/java/io/github/ashishbg/springsecurityjpa/SpringSecurityJpaApplication.java
+++ src/main/java/io/github/ashishbg/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -2,8 +2,10 @@ package io.github.ashishbg.springsecurityjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "io.github.ashishbg.springsecurityjpa.repository")
 public class SpringSecurityJpaApplication {
 
 	public static void main(String[] args) {
